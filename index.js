51868958a7255773f60ef943e29c7af1f4ddf757
Vinícius Lima Ferreira <101911986+vinicius0919@users.noreply.github.com>
2025-08-20 09:27:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const { Server } = require("socket.io");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 const server = createServer(app);
 
 require("./db/connection");
